ba82b77301c0f1dabd4cce111b2b4c4e8974c028
Yurii Ushakov <lures232@gmail.com>
2021-03-27 00:57:02 +0200

    add parcing title and description of films

--- http_requests.py
+++ http_requests.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from bs4 import BeautifulSoup
 
 
 def send_http_request(url):
@@ -10,5 +11,19 @@ def send_http_request(url):
         print('Invalid quote resource!')
 
 
-user_input = input('Input the URL:\n')
-send_http_request(user_input)
+def movie_title_and_description(url):
+    req = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+    if req:
+        soup = BeautifulSoup(req.content, "html.parser")
+
+        description = soup.find('div', {'class': "summary_text"})
+        if description:
+            movie_d = {"title": soup.title.text, "description": description.text.strip()}
+            print(movie_d)
+        else:
+            print('Invalid movie page!')
+
+
+user_input = "https://www.imdb.com/title/tt0080684/"
+# send_http_request(user_input)
+movie_title_and_description(user_input)
